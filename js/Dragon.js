a5fa9a362b3a1b71e13bb0e5a8bd6b833cdbba52
Jamie <jamie@mvcodeclub.com>
2020-02-09 21:56:09 -0800

    centeredFireThirdtime

--- js/Dragon.js
+++ js/Dragon.js
@@ -32,7 +32,7 @@ export default class Dragon {
   shootFire() {
     if (!this.canShoot) {return;}
     if (this.currentFlame >= this.flameCost) {
-      let dir = Math.atan2(this.aimY - this.y, this.aimX - this.x);
+      let dir = Math.atan2(this.aimY - this.y - 64, this.aimX - this.x - 48);
       this.flames.push(new Flame(this.x + 64, this.y + 48, dir));
       this.currentFlame -= this.flameCost;
     }
