e49007a9336af6fb51359cea5353fade4e9a3d97
BuildTools <sralinkous@gmail.com>
2020-02-09 19:41:39 -0800

    Commit for merge

--- js/Knight.js
+++ js/Knight.js
@@ -1,34 +1,29 @@
-let knightImage = document.querySelector("#knight");
-
 class Knight {
   constructor(x, y, speed = 1) {
     this.x = x;
     this.y = y;
-    this.width = 32;
-    this.height = 32;
     this.num = 0;
     this.speed = speed;
   }
 
   draw(ctx) {
-    ctx.drawImage("knightImage", this.x, this.y, this.width, this.height);
+    ctx.drawImage();
   }
 
   update() {
-    this.movePath();
+      movePath();
   }
 
+
+
   movePath() {
-    let distx = path[this.num].x - this.x;
-    let disty = path[this.num].y - this.y;
+    let dx = path[this.num].x - this.x;
+    let dy = path[this.num].y - this.y;
 
-    let dx = min(this.speed, distx) * sign(distx);
-    let dy = min(this.speed, disty) * sign(disty);
-
-    if (distx > this.speed / 2 && placeFree(this.x + dx, this.y)) {
-      this.x += dx;
-    } else if (disty > this.speed / 2 && placeFree(this.x, this.y + dy)) {
-      this.y += dy;
+    if (dx > this.speed / 2) {
+      this.x += min(this.speed, dx) * sign(dx);
+    } else if (dy > this.speed / 2) {
+      this.y += min(this.speed, dy) * sign(dy);
     }
   }
 }
@@ -36,5 +31,3 @@ class Knight {
 function sign(num) {
   return (num == 0) ? 0 : ((num > 0) ? 1 : -1);
 }
-
-export default class Knight;
